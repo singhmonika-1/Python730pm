f1b2a134dc94a9382ca1bac7a19c2ee5737d8ada
singhmonika-1 <singhmonikasingh04@gmail.com>
2026-06-10 20:13:04 +0530

    Day 3-list

---
+++ Day3_loops(list).py
@@ -0,0 +1,44 @@
+# List
+# Loops
+# for x in range(4):
+#     print(x)
+
+# for x in range(1,5):
+#     print(x)    
+
+# for x in range(7):
+#     print(x)
+
+# for x in range(1,10,2):
+#     print(x)    
+
+# for x in range(2,20,4):
+#     print(x)   
+
+# # parameters
+# # start --- starting value(included)   default value =0
+# # stop -----Ending value(Excluded) 
+# # Step -----Increment or decrement value  default value =1
+
+# for x in range(2,8,2):
+#     print(x)# 2,4,6
+# #             0       1        2      3
+# countries = ["Cuba","India","France","USA"]   
+# for x in range(4):
+#     print(x) 
+#     print(countries[x])
+
+# fruits = ["apple","Mango","Banana","Grapes","Guava"]    
+# for x in range(5):
+#     print(fruits[x])
+
+Cities = ["Pune","Delhi","Mumbai","kolkata","Agra","Jaipur"]
+# for x in range(6):
+#     print(Cities[x]) 
+
+print(Cities[1])
+
+print(Cities[4])
+
+for item in Cities:
+    print(item)
