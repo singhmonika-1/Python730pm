92793d047116a7abf4703a40a1c354578771ff1c
singhmonika-1 <singhmonikasingh04@gmail.com>
2026-07-02 20:16:38 +0530

    Day13-strings

---
+++ Day13_methods.py
@@ -0,0 +1,42 @@
+# Methods
+
+str = "learning Python programming"
+
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+print(str.title())
+print(str.swapcase())
+
+# check methods-----Boolean value(True/False)
+print("python".isalpha())
+print("12".isalpha())
+print("1234".isdigit())
+print("123aa".isalnum()) # alpha, numbers, alphanum
+print("555".isalnum())
+print("aaa".isalnum())
+print(" ".isspace())
+print("".isspace())
+print("PYTHON".isupper())
+print("python".islower())
+print("I am Learning Python".istitle())
+print("Hello".startswith("H"))
+print("Hello".startswith("h"))  # isstartswith() is case-sensitive
+print("Excel".endswith("l"))
+print("Excel".endswith("L"))
+
+# Identifier
+# Identifier is a name that identifies a variable, function name, class or module
+
+
+# a string is a valid identifier if
+
+# 1.It contains only (letters (A-Z),  Digits (0-9) , underscore(_))
+# 2. It does not start with a digit
+# 3.It does not contain spaces.
+# 4. It does not contain special characters like #,@,$,%,&,-,= etc
+# 5. It is not empty.
+
+
+
+
