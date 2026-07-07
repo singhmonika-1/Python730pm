93736918a36101fd3935dafac32835b17a8ab516
singhmonika-1 <singhmonikasingh04@gmail.com>
2026-07-07 20:14:37 +0530

    Day15 - strings

---
+++ Day15_methods.py
@@ -0,0 +1,107 @@
+# not operator
+#True = False
+# False = True
+
+# not operator is a logical operator used to reverse the condition's truth value
+
+# print(not(4 == 4))
+# #     not (True)
+# #        False
+
+# print(not(4 >6))
+# #      not (False)
+# #       True
+
+# print(not(8 == 8 and 8>9))
+# #     not( True  and  False)
+# #     not(False)
+# #      True
+
+# print(not( 8 == 8 or 8>9))
+# #    not( True  or False)
+# #    not( True)
+# #     False
+
+# check methods ----- boolean value
+print("python".isalpha())
+print("123".isdigit())
+print("aaa".isdigit())
+print("aa123".isalnum()) # alphabets , numbers, alphanumbers
+print("rrt".isalnum())
+print("345".isalnum())
+print(" ".isspace())
+print("56 ".isspace())
+print("javascript".islower())
+print("PYTHON".islower())
+print("PYTHON".isupper())
+print("Java Script".istitle())
+print("Hello".startswith("H"))
+print("Hello".startswith("h"))  # startswith is case-sensitive
+print("Hello".endswith("o"))
+print("ad_123".isidentifier())
+print("2_ad".isidentifier())
+
+
+
+# Identifier
+# Identifier is a name that identifies a variable, function name, class or module
+
+
+# a string is a valid identifier if
+
+# 1.It contains only (letters (A-Z),  Digits (0-9) , underscore(_))
+# 2. It does not start with a digit
+# 3.It does not contain spaces.
+# 4. It does not contain special characters like #,@,$,%,&,-,= etc
+# 5. It is not empty.
+
+
+# st = "Python"
+# print(st)
+
+# 1st = "java"
+# print(1st)   # variable name cannot be started with numbers
+
+# _st = [1,2,3]
+# print(_st)
+
+
+# split
+s = "apple- banana-orange-mango"
+e = s.split("-")
+print(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Identifier
+# Identifier is a name that identifies a variable, function name, class or module
+
+
+# a string is a valid identifier if
+
+# 1.It contains only (letters (A-Z),  Digits (0-9) , underscore(_))
+# 2. It does not start with a digit
+# 3.It does not contain spaces.
+# 4. It does not contain special characters like #,@,$,%,&,-,= etc
+# 5. It is not empty.
